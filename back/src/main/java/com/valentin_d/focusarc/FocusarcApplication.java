e64de75c0f78325ce76022fae070ecc9e561a28b
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2026-02-15 13:30:47 +0100

    base commit

---
+++ back/src/main/java/com/valentin_d/focusarc/FocusarcApplication.java
@@ -0,0 +1,13 @@
+package com.valentin_d.focusarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusarcApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/valentin_d/focusarc/FocusarcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentin_d.focusarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusarcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
