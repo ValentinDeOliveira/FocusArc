b7cd36309e314f0b2052a8c8ad0c84552dc4620b
ValentinDeOliveira <52882895+ValentinDeOliveira@users.noreply.github.com>
2026-02-19 16:31:29 +0100

    Setup ci workflow (#2)
    setup ci workflow by adding ci.yml.
    Fixed a few test + mongo authentification

--- back/src/test/java/com/valentin_d/focusarc/repository/UserRepositoryTest.java
+++ back/src/test/java/com/valentin_d/focusarc/repository/UserRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.valentin_d.focusarc.repository;
 
 import com.valentin_d.focusarc.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataMongoTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
     @Autowired
     private UserRepository repository;
@@ -23,5 +26,10 @@ class UserRepositoryTest {
         final var foundUser = userOptional.get();
         assertEquals(foundUser.getEmail(), user.getEmail());
         assertEquals(foundUser.getName(), user.getName());
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
     }
 }
