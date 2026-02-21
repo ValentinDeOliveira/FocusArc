7455a4b01f38b22a7d64412ea2816846488f641c
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2026-02-21 22:29:17 +0100

    added validator exception

--- back/src/test/java/com/valentin_d/focusarc/exception/handler/ValidationExceptionHandlerTest.java
+++ back/src/test/java/com/valentin_d/focusarc/exception/handler/ValidationExceptionHandlerTest.java
@@ -1,6 +1,5 @@
 package com.valentin_d.focusarc.exception.handler;
 
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.constraints.NotBlank;
@@ -9,7 +8,6 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,17 +33,17 @@ class ValidationExceptionHandlerTest {
         try (final var factory = Validation.buildDefaultValidatorFactory()) {
             final var validator = factory.getValidator();
 
-            Set<ConstraintViolation<TestDto>> violations = validator.validate(dto);
+            final var violations = validator.validate(dto);
             final var exception = new ConstraintViolationException(violations);
 
-            var response = handler.handleValidationErrors(exception);
+            final var response = handler.handleValidationErrors(exception);
 
             assertEquals(400, response.getStatusCode().value());
             assertNotNull(response.getBody());
 
-            var errors = (List<?>) response.getBody().get("errors");
+            final var errors = (List<?>) response.getBody().get("errors");
             assertEquals(1, errors.size());
-            var firstError = (Map<?, ?>) errors.get(0);
+            final var firstError = (Map<?, ?>) errors.get(0);
 
             assertEquals("name", firstError.get("field"));
             assertEquals("", firstError.get("rejectedValue"));
