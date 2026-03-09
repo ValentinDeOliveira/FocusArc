7ca5492dcdc988d43764c91023f951bdecd4d442
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2026-03-09 16:14:45 +0100

    initialize front by adding basic services

--- back/src/main/java/com/valentin_d/focusarc/dto/arc/ArcUpdateDto.java
+++ back/src/main/java/com/valentin_d/focusarc/dto/arc/ArcUpdateDto.java
@@ -1,6 +1,7 @@
 package com.valentin_d.focusarc.dto.arc;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
-public record ArcUpdateDto(String name, @Positive Integer totalEstimatedMinutes) {
+public record ArcUpdateDto(@NotBlank String name, @Positive Integer totalEstimatedMinutes) {
 }
