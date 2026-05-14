253fdf7f032f0809aba069be695be0b52b25430c
ValentinDeOliveira <valentindeoliveira63@gmail.com>
2026-05-14 08:49:56 +0200

    tweaking css of duration to make it aligned with start time

--- front/src/app/shared/task-time-duration/task-time-duration.ts
+++ front/src/app/shared/task-time-duration/task-time-duration.ts
@@ -6,7 +6,9 @@ import {TimeField} from '../time-field/time-field';
     selector: 'app-task-time-duration',
     imports: [NumberField, TimeField],
     templateUrl: './task-time-duration.html',
-    styleUrl: './task-time-duration.css',
+    styleUrls: [
+        './task-time-duration.css',
+    ]
 })
 export class TaskTimeDuration {
     startTime = model<string>('09:00');
